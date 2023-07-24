7c6376e9fdfaa29ac9e3f98876873618c981bbdb
nadeemb53 <nadeemb53@gmail.com>
2023-07-25 02:08:11 +0530

    changes

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -121,7 +121,7 @@ export default function Zigzag() {
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Generate invoices and manage salary slips, contract invoices as a task all at the same place</span>
+                      <span>Generate invoices, manage salary slips and pay contractor invoices as a task all at the same place</span>
                     </li>
                   </ul>
                 </div>
