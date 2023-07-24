ed5b1846b3363a9ed069dbc919d4d35b02224463
nadeemb53 <nadeemb53@gmail.com>
2023-07-24 23:31:17 +0530

    fixes

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = true;
-
-let assetPrefix = "";
-let basePath = "/";
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  //   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  const repo = "https://orro-tech.github.io";
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: "imgix",
-    path: "orro-tech.imgix.net",
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
